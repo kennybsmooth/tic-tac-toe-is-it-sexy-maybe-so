a2a786aa8ff5a8994d4ec02ca24c60709ef13f32
kennybsmooth <kennybui@Kennys-MacBook-Air.local>
2016-11-24 17:17:13 -0500

    Created a function to set turns.

--- ttt.js
+++ ttt.js
@@ -1,9 +1,24 @@
-var player1Name="" , player2Name="", turn = "";
+var player1Name="Danny-D" , player2Name="Kenny-B", turn = "";
 var grid =  [[0,0,0],[0,0,0],[0,0,0]];
 var hasWinner = 0, moveCount=0;
 
 function boardMsg(x){
     return $("#board").text(x);
+}
+
+//
+
+function setTurn(){
+    var r = Math.floor((Math.random() * 2) + 1);
+    hasWinner=0;
+    if(r==1){
+        turn = player1Name;
+        boardMsg(player1Name+"'s turn now!");
+    }
+    else{
+        turn = player2Name;
+        boardMsg(player2Name+"'s turn now!");
+    }
 }
 
 //
